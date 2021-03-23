3daadbd5a1eb104d05f2abb074145794aabbe09a
James Wyn <thisisjameswyn@gmail.com>
2021-03-23 14:15:08 -0700

    add initial setup files and complete logic
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#javascript h2").click(function() {
+    $("#javascript p").toggle();
+  });
+  $("#operators h2").click(function() {
+    $("#operators p").toggle();
+  })
+  $("#variables h2").click(function() {
+    $("#variables p").toggle();
+  })
+  $("#functions h2").click(function() {
+    $("#functions p").toggle();
+  })
+  $("#methods h2").click(function() {
+    $("#methods p").toggle();
+  })
+  $("#arguments h2").click(function() {
+    $("#arguments p").toggle();
+  })
+  $("#parameters h2").click(function() {
+    $("#parameters p").toggle();
+  })
+  $("#return h2").click(function() {
+    $("#return p").toggle();
+  })
+});
